e4d9f7ec647973ba4f1502d1589cd92105d27aa3
Neye Emmanuel <emmanuelneye@gmail.com>
2018-01-24 11:08:07 -0500

    CardView Import the library

---
+++ AndroidStudioProjects/IntentsPractice/app/src/main/java/com/example/knez/intentspractice/Constants.java
@@ -0,0 +1,12 @@
+package com.example.knez.intentspractice;
+
+/**
+ * Created by KNEZ on 12/20/2017.
+ */
+
+public class Constants{
+
+    public static final String KEY_NAME = "name";
+
+    public static final String KEY_AGE = "age";
+}
---
+++ AndroidStudioProjects/IntentsPractice/app/src/main/java/com/example/knez/intentspractice/SecondActivity.java
@@ -0,0 +1,75 @@
+package com.example.knez.intentspractice;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        TextView secondActivityTextView = (TextView) findViewById(R.id.secondActivityTextView);
+    }
+    @Override
+    protected void onRestart() {
+        Log.i(TAG, "onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(TAG, "onStart");
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "onResume");
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+}
